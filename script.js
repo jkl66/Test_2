824a8c3d1ca1017fb3a00dca65596366781effb9
jkl66 <46427917+jkl66@users.noreply.github.com>
2019-03-03 12:43:00 +0200

    Test_2

---
+++ script.js
@@ -0,0 +1,37 @@
+
+///POPUP///
+var modal = document.querySelector(".modal");
+    var trigger = document.querySelector(".trigger");
+    var closeButton = document.querySelector(".close-button");
+
+    function toggleModal() {
+        modal.classList.toggle("show-modal");
+    }
+
+    function windowOnClick(event) {
+        if (event.target === modal) {
+            toggleModal();
+        }
+    }
+
+    trigger.addEventListener("click", toggleModal);
+    closeButton.addEventListener("click", toggleModal);
+    window.addEventListener("click", windowOnClick);
+
+    //ACCORDION//
+
+  $('.plus').click(function(){
+    $('.fold').toggleClass('.open')
+  });
+
+
+  ///AJAX//
+  
+$.ajax({
+    url: 'https://randomuser.me/api/',
+    dataType: 'json',
+    success: function(data) {
+      console.log(data);
+    }
+  });
+
